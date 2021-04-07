275cc04a2c9dad9cd5fe90e57040d4d07edd6aa5
Ruman Hossain <ruman.cse.brur@gmail.com>
2021-04-07 23:06:59 +0600

    Media File in Django Admin

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
 
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
 
@@ -8,4 +10,4 @@ urlpatterns = [
     path('',include('index.urls')),
     path('employee/', include('employee.urls')),
 
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
