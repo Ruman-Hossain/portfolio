8943a012563d80685b5ea9ade0d6713ed34dced5
Ruman Hossain <ruman.cse.brur@gmail.com>
2021-04-07 18:34:55 +0600

    Root App Chane and Tree Structure URL formed with other apps

---
+++ index/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def profile(request):
+    return render(request,'contact.html')
